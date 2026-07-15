dbb70cf41ae7f651a1c4e519f6317ceca00795c3
Marketshopdev <k8926675@gmail.com>
2026-07-15 12:08:51 +0000

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCCUMMtsuDBU7eJf7-A4Y2dXoh2M0_lCCM",
+  authDomain: "market-shop-site-b1542.firebaseapp.com",
+  projectId: "market-shop-site-b1542",
+  storageBucket: "market-shop-site-b1542.firebasestorage.app",
+  messagingSenderId: "365211975254",
+  appId: "1:365211975254:web:66bb7d07cce5fac4dec5f7"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
